f5386031e3dc40eb3ec28215d1fd785375d17445
regis066 <mugisharegis72@gmail.com>
2024-10-09 20:55:33 +0200

    Add Book services(Business logic)

--- src/main/java/com/project/book_review_api/service/BookService.java
+++ src/main/java/com/project/book_review_api/service/BookService.java
@@ -1,4 +1,47 @@
 package com.project.book_review_api.service;
 
+import com.project.book_review_api.model.Book;
+import com.project.book_review_api.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book updateBook(Long id, Book bookDetails) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found with id: " + id));
+        book.setTitle(bookDetails.getTitle());
+        book.setAuthor(bookDetails.getAuthor());
+        book.setReview(bookDetails.getReview());
+
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found with id: " + id));
+
+        bookRepository.deleteById(id);
+    }
 }
