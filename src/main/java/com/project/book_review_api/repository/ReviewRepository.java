9bf12340bbac096f5c0f855daf7162a06080cd67
regis066 <mugisharegis72@gmail.com>
2024-10-09 20:54:38 +0200

    Add Review Repository

---
+++ src/main/java/com/project/book_review_api/repository/ReviewRepository.java
@@ -0,0 +1,12 @@
+package com.project.book_review_api.repository;
+
+import com.project.book_review_api.model.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByBookId(Long bookId);
+}
